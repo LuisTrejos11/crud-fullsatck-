4f3ca2a49691360cb1dec9f89265a7a764580eeb
LuisTrejos11 <54832126+LuisTrejos11@users.noreply.github.com>
2022-01-21 20:09:44 -0600

    Se agrega el proyecto

---
+++ FBTarjeta/AplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FBTarjeta.Models;
+
+namespace FBTarjeta
+{
+    public class AplicationDBContext: DbContext
+    {
+       public  DbSet<TarjetaCredito> TaretaCredito { get; set; }
+        public AplicationDBContext(DbContextOptions<AplicationDBContext> options) : base(options)
+        { 
+            
+        }
+    }
+}
---
+++ tarjetaCredito/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TarjetaCreditoComponent } from './components/tarjeta-credito/tarjeta-credito.component';
+
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+
+import {HttpClientModule} from '@angular/common/http'; 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TarjetaCreditoComponent
+  ],
+  imports: [
+    BrowserModule, 
+    ReactiveFormsModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
